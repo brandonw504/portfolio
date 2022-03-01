bfd1eeba88326f6718d8de1e76c45ef2dc10a664
brandonw504 <71607013+brandonw504@users.noreply.github.com>
2022-02-28 16:24:39 -0800

    updated graduation

--- pages/education.js
+++ pages/education.js
@@ -14,12 +14,12 @@ function Education() {
             <h2 data-aos="fade-right" data-aos-once="true">Education</h2>
             <h3 data-aos="fade-right" data-aos-once="true">Bachelor of Computer Science (in progress)</h3>
             <h4 data-aos="fade-right" data-aos-once="true">University of California, Davis</h4>
-            <p data-aos="fade-right" data-aos-once="true">&emsp;Expected Graduation: Winter 2024</p>
+            <p data-aos="fade-right" data-aos-once="true">&emsp;Expected Graduation: June 2024</p>
             <p data-aos="fade-right" data-aos-once="true">&emsp;GPA: 3.79</p>
 
             <h3 data-aos="fade-right" data-aos-once="true">High School</h3>
             <h4 data-aos="fade-right" data-aos-once="true">Evergreen Valley High School</h4>
-            <p data-aos="fade-right" data-aos-once="true">&emsp;Valedictorian - Graduated Spring 2021</p>
+            <p data-aos="fade-right" data-aos-once="true">&emsp;Valedictorian - Graduated June 2021</p>
             <p data-aos="fade-right" data-aos-once="true">&emsp;GPA: 4.41 weighted, 4.00 unweighted</p>
         </div>
     )
